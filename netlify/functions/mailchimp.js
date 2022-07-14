0376661efdaf27b21b16009fe0920f7360bf73cc
odusanya <tommyodus@gmail.com>
2022-07-14 23:36:32 +0100

    chore: move mailchimp helper to async

--- netlify/functions/mailchimp.js
+++ netlify/functions/mailchimp.js
@@ -8,87 +8,95 @@ const mailChimpListID = process.env.MAILCHIMP_AUDIENCE_ID;
 const mailChimpServer = process.env.MAILCHIMP_SERVER;
 
 
-exports.handler = (event, context, callback) => {
+exports.handler = async (event) => {
   let body = {}
   body = JSON.parse(event.body)
 
   if (!body.email) {
     console.log('missing email')
-    return callback(null, {
+    return {
       statusCode: 400,
       body: JSON.stringify({
         error: 'missing email'
       })
-    })
+    }
   }
 
   if (!mailChimpAPI) {
     console.log('missing mailChimpAPI key')
-    return callback(null, {
+    return {
       statusCode: 400,
       body: JSON.stringify({
         error: 'missing mailChimpAPI key'
       })
-    })
+    }
   }
 
   if (!mailChimpListID) {
     console.log('missing mailChimpListID key')
-    return callback(null, {
+    return {
       statusCode: 400,
       body: JSON.stringify({
         error: 'missing mailChimpListID key'
       })
-    })
+    }
   }
 
-  const data = {
-    email_address: body.email,
-    status_if_new: "pending",
-    merge_fields: {}
-  };
   const emailHash = crypto.createHash('md5').update(body.email).digest('hex');
-  const subscriber = JSON.stringify(data);
+  // const subscriber = (data);
   const url = `https://${mailChimpServer}.api.mailchimp.com/3.0/lists/${mailChimpListID}/members/${emailHash}`;
-
-  axios(
-    {
-      method: 'put',
-      url,
-      data: subscriber,
-      auth: {
-        username: 'apikey',
-        password: mailChimpAPI
-      }
+  const config = {
+    method: 'put',
+    url,
+    data: JSON.stringify({
+      email_address: body.email,
+      status_if_new: "pending"
+    }),
+    auth: {
+      username: 'key',
+      password: mailChimpAPI
     }
-  ).then(function(response){
+  }
+  try {
+    const response = await axios(config);
+    const { email_address, status } = response.data;
 
-    // Return data to AJAX request
-    return callback(null, {
-      statusCode: 200,
-      body: JSON.parse({ ok: true, data: {
-        status: response.data.status,
-        email: response.data.email_address,
-      } })
-    })
-  }).catch(function(error) {
+    return {
+      statusCode: response.status || 200,
+      body: JSON.stringify({
+        ok: true,
+        data: {
+          email: email_address,
+          status,
+        }
+      })
+    }
+
+  } catch (error) {
+
     if (error.response) {
       // The request was made and the server responded with a status code
       // that falls out of the range of 2xx
-      return callback(null, {
-            statusCode: 400,
-            body: JSON.stringify({ ok: true, data: error.response.data })
-        })
+      console.log(error.response.status, 'out of range');
+      return {
+        statusCode: 400,
+        body: JSON.stringify({ ok: true, data: error?.response?.data })
+      }
     } else if (error.request) {
       // The request was made but no response was received
       console.log(error.request, 'error.request');
-    } else {
-      // Something happened in setting up the request that triggered an Error
-      console.log('Error', error.message);
-    }
-    return callback(null, {
+      return {
         statusCode: 400,
-        body: JSON.stringify({ ok: true, data: error.response.data })
-    })
-  });
+        body: JSON.stringify({ ok: true, data: error?.response?.data })
+      }
+    } else {
+      return {
+        statusCode: 500,
+        body: JSON.stringify({
+          ok: false,
+          ...error
+        })
+      }
+    }
+  }
 };
