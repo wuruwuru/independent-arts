49dad11dbc21d8918d7cf81b3086038912407605
Opemipo Aikomo <opemipoaikomo@gmail.com>
2024-10-04 17:55:06 +0100

    Fix css in prod

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,15 @@
 import { defineConfig } from 'astro/config';
 import sitemap from "@astrojs/sitemap";
+import path from 'path';
 
 export default defineConfig({
   site: 'https://independent-arts.org',
-  integrations: [sitemap()]
+  integrations: [sitemap()],
+  vite: {
+    resolve: {
+      alias: {
+        '@styles': path.resolve('./src/styles'),
+      },
+    },
+  },
 });
